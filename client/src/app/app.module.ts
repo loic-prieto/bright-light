ca59aa74ac9bf87de978c2250053ad1ebe17fea4
Loic Prieto <l.prieto@ocado.com>
2021-11-22 11:49:29 +0100

    Add default angular client to project
    Angular is chosen as the framework to build the web app client. This commit adds
    a base angular application with a predefined set of files and configs, and also
    adds the skeleton of a roster view component, that is part of the goals of the
    milestone alpha 1.

---
+++ client/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RosterComponent } from './roster/roster.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RosterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }

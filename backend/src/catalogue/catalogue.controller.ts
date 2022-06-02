928a78e05dbc94ccf93aae77d7c48ef753eee3d4
Loïc Prieto <loic@the-fire-archmage.ninja>
2022-06-02 20:51:44 +0200

    Refine code auto-formatters and switch zip library
    The promise-based JSZip was causing a lot of ugliness when interacting with Observables, and this use case did not really involve async while loading the zip file itself from memory. amd-zip is a better fit for our needs in this case.

--- backend/src/catalogue/catalogue.controller.ts
+++ backend/src/catalogue/catalogue.controller.ts
@@ -1,24 +1,31 @@
-import { Controller, Get, Res, HttpStatus } from '@nestjs/common';
-import { Response } from 'express';
-import { CatalogueService } from './catalogue.service';
-import { CatalogueIndex } from 'bright-light-common';
+import { Controller, Get, Res, HttpStatus } from "@nestjs/common";
+import { Response } from "express";
+import { CatalogueService } from "./catalogue.service";
+import { CatalogueIndex } from "bright-light-common";
 
-@Controller('catalogues')
+@Controller("catalogues")
 export class CatalogueController {
   static BS_DATA_INDEX =
-    'https://battlescribedata.appspot.com/repos/wh40k/index.bsi';
+    "https://battlescribedata.appspot.com/repos/wh40k/index.bsi";
 
   constructor(private readonly catalogueService: CatalogueService) {}
 
   @Get()
   async getCatalogueList(@Res() response: Response<CatalogueIndex | Error>) {
-    return this.catalogueService
-      .getCatalogueList()
-      .ifLeft((error: Error) => {
-        response.status(HttpStatus.INTERNAL_SERVER_ERROR).json(error).send();
-      })
-      .ifRight((catalogues: CatalogueIndex) => {
-        response.status(HttpStatus.OK).json(catalogues).send();
-      });
+    return this.catalogueService.getCatalogueList().subscribe({
+      next: (catalogueIndex) =>
+        catalogueIndex
+          .ifLeft((error: Error) => {
+            response
+              .status(HttpStatus.INTERNAL_SERVER_ERROR)
+              .json(error)
+              .send();
+          })
+          .ifRight((catalogues: CatalogueIndex) => {
+            response.status(HttpStatus.OK).json(catalogues).send();
+          }),
+      error: (e) =>
+        response.status(HttpStatus.INTERNAL_SERVER_ERROR).json(e).send(),
+    });
   }
 }
--- backend/src/catalogue/catalogue.service.ts
+++ backend/src/catalogue/catalogue.service.ts
@@ -1,29 +1,28 @@
-import { HttpStatus } from '@nestjs/common';
-import { Injectable } from '@nestjs/common';
-import { Either, Left, Right } from 'purify-ts/Either';
-import { map, catchError, concatMap } from 'rxjs/operators';
-import { HttpService } from '@nestjs/axios';
-import JSZip from 'jszip';
-import { loadAsync } from 'jszip';
-import { BattleScribeIndex } from './model/battlescribe/index/BattleScribeIndex';
-import { CatalogueIndex } from '../../../common/src/model/CatalogueIndex';
-import { Observable, pipe } from 'rxjs';
-import { EitherAsync } from 'purify-ts';
+import { HttpStatus, Injectable } from "@nestjs/common";
+import { Either, Left, Right } from "purify-ts/Either";
+import { map } from "rxjs/operators";
+import { HttpService } from "@nestjs/axios";
+import { CatalogueIndex } from "../../../common/src/model/CatalogueIndex";
+import { Observable } from "rxjs";
+import { BattleScribeIndex } from "./model/battlescribe/index/BattleScribeIndex";
+
+const zip = require("amd-zip");
 
 @Injectable()
 export class CatalogueService {
   static BS_DATA_INDEX =
-    'https://battlescribedata.appspot.com/repos/wh40k/index.bsi';
+    "https://battlescribedata.appspot.com/repos/wh40k/index.bsi";
 
   constructor(private readonly httpClient: HttpService) {}
 
   getCatalogueList(): Observable<Either<Error, CatalogueIndex>> {
     return this.httpClient
       .get(CatalogueService.BS_DATA_INDEX, {
-        responseType: 'arraybuffer',
-        responseEncoding: 'binary',
+        responseType: "arraybuffer",
+        responseEncoding: "binary",
       })
       .pipe(
+        // Convert http response into an Either of raw bytes
         map((response) => {
           let result: Either<Error, ArrayBuffer>;
           if (response.status == HttpStatus.OK) {
@@ -31,29 +30,37 @@ export class CatalogueService {
           } else {
             result = Left(
               new Error(
-                `There was an error while retrieving the catalogue index: ${response.status}-${response.data}`,
-              ),
+                `There was an error while retrieving the catalogue index (${CatalogueService.BS_DATA_INDEX}): ${response.status}-${response.data}`
+              )
             );
           }
           return result;
         }),
-      )
-      .pipe(
-        map(rawResult=>Right("")}
-        //concatMap((rawResult: Either<Error, ArrayBuffer>) => {
-        //  return rawResult
-        //    .map(loadAsync)
-        //    .map((zipFile: JSZip) => zipFile.file('index.xml').async('string'));
-        //}),
-      )
-      .pipe(
-        map((xmlText: Either<Error, string>) => {
-          return xmlText
-            .map(BattleScribeIndex.fromString)
+        // Convert Either of raw bytes into Either of the content of the BS index file inside the zip archive that the
+        // raw bytes represent
+        map((zipfileBytes) =>
+          zipfileBytes.map((bytes) => {
+            const zipFile = zip(bytes);
+            const indexFile = zipFile.getEntry("index.xml");
+            if (indexFile) {
+              return indexFile.getData().toString("utf8") as string;
+            } else {
+              throw new Error(
+                `The downloaded battlescribe index archive (${CatalogueService.BS_DATA_INDEX}) did not have an index.xml file inside`
+              );
+            }
+          })
+        ),
+        // Convert the index xml text representation to a catalogue index
+        map((indexText) =>
+          indexText
+            .map((indexContent) =>
+              BattleScribeIndex.fromString(indexContent)
+                .map((bsIndex) => bsIndex.toCatalogueIndex())
+                .join()
+            )
             .join()
-            .map((catalogue) => catalogue.toCatalogueIndex())
-            .join();
-        }),
+        )
       );
   }
 }

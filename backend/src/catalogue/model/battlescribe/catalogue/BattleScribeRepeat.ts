fe501fa89792888273164e9d5a58a2516521efdd
Loïc Prieto <loic@the-fire-archmage.ninja>
2022-06-02 21:17:24 +0200

    Output ts compilation to dist folder

--- backend/src/catalogue/model/battlescribe/catalogue/BattleScribeRepeat.ts
+++ backend/src/catalogue/model/battlescribe/catalogue/BattleScribeRepeat.ts
@@ -1,5 +1,5 @@
-import { XML } from 'sxml';
-import { getBool, getNumber } from '../../../util/sxml-utils';
+import { XML } from "sxml";
+import { getBool, getNumber } from "../../../../util/sxml-utils";
 
 export class BattleScribeRepeat {
   constructor(
@@ -12,21 +12,21 @@ export class BattleScribeRepeat {
     public includeChildForces: boolean,
     public childId: string,
     public repeats: number,
-    public roundUp: boolean,
+    public roundUp: boolean
   ) {}
 
   static fromXMLNode(xmlNode: XML): BattleScribeRepeat {
     return new BattleScribeRepeat(
-      xmlNode.getProperty('field'),
-      xmlNode.getProperty('scope'),
-      xmlNode.getProperty('value'),
-      xmlNode.getProperty('percentValue'),
-      getBool('shared', xmlNode),
-      getBool('includeChildSelections', xmlNode),
-      getBool('includeChildForces', xmlNode),
-      xmlNode.getProperty('childId'),
-      getNumber('repeats', xmlNode),
-      getBool('roundUp', xmlNode),
+      xmlNode.getProperty("field"),
+      xmlNode.getProperty("scope"),
+      xmlNode.getProperty("value"),
+      xmlNode.getProperty("percentValue"),
+      getBool("shared", xmlNode),
+      getBool("includeChildSelections", xmlNode),
+      getBool("includeChildForces", xmlNode),
+      xmlNode.getProperty("childId"),
+      getNumber("repeats", xmlNode),
+      getBool("roundUp", xmlNode)
     );
   }
 }
